e0f398643e8df76b71df9ba2c6048ec66d62bbc0
Amit <mexindian@gmail.com>
2019-02-14 09:40:33 +0000

    adding some tests and plotting SIN (broken)

--- R/functions.R
+++ R/functions.R
@@ -9,9 +9,9 @@
 #' @details DETAILS
 #' @examples
 #' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
+#' if (interactive()) {
+#'   # EXAMPLE1
+#' }
 #' }
 #' @seealso
 #'  \code{\link[syuzhet]{get_sentences}},\code{\link[syuzhet]{get_sentiment}},\code{\link[syuzhet]{get_nrc_sentiment}}
@@ -19,7 +19,7 @@
 #' @export
 #' @importFrom syuzhet get_sentences get_sentiment get_nrc_sentiment
 #' @importFrom dplyr %>% select mutate
-emoDataframeMaker <- function(text, sentimentType = "syuzhet", addColor = FALSE, nrc = FALSE){
+emoDataframeMaker <- function(text, sentimentType = "syuzhet", addColor = FALSE, nrc = FALSE) {
   ## Error catchers
   if (class(text) != "character") stop("Need a character vector")
   if (length(text) != 1) stop("I only take one text at a time. To feed me many, purrr:map me")
@@ -28,19 +28,22 @@ emoDataframeMaker <- function(text, sentimentType = "syuzhet", addColor = FALSE,
     mutate(cumSentiment = cumsum(sentiment))
   if (addColor) a <- a %>% mutate(color = case_when(sentiment >= 0 ~ "green", TRUE ~ "red"))
 
-  if (nrc) a <- bind_cols(a, syuzhet::get_nrc_sentiment(a$text)) %>%
-    mutate(cumAnger = cumsum(anger),
-           cumAnticipation = cumsum(anticipation),
-           cumDisgust = cumsum(disgust),
-           cumFear = cumsum(fear),
-           cumJoy = cumsum(joy),
-           cumSadness = cumsum(sadness),
-           cumSurprise = cumsum(surprise),
-           cumTrust = cumsum(trust)
-           # ,
-           # cumNegative = cumsum(negative),
-           # cumPositive = cumsum(positive)
-           )
+  if (nrc) {
+    a <- bind_cols(a, syuzhet::get_nrc_sentiment(a$text)) %>%
+      mutate(
+        cumAnger = cumsum(anger),
+        cumAnticipation = cumsum(anticipation),
+        cumDisgust = cumsum(disgust),
+        cumFear = cumsum(fear),
+        cumJoy = cumsum(joy),
+        cumSadness = cumsum(sadness),
+        cumSurprise = cumsum(surprise),
+        cumTrust = cumsum(trust)
+        # ,
+        # cumNegative = cumsum(negative),
+        # cumPositive = cumsum(positive)
+      )
+  }
   a
 }
 
@@ -52,9 +55,9 @@ emoDataframeMaker <- function(text, sentimentType = "syuzhet", addColor = FALSE,
 #' @details DETAILS
 #' @examples
 #' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
+#' if (interactive()) {
+#'   # EXAMPLE1
+#' }
 #' }
 #' @seealso
 #'  \code{\link[broom]{tidy}},\code{\link[broom]{glance}}
@@ -63,14 +66,14 @@ emoDataframeMaker <- function(text, sentimentType = "syuzhet", addColor = FALSE,
 #' @export
 #' @importFrom broom tidy glance
 #' @importFrom rlang syms
-slopeFinder <- function(emoDF){
+slopeFinder <- function(emoDF) {
   ## linear
   dataToModel <- emoDF %>% pull(cumSentiment) %>% data_frame(y = .) %>% mutate(x = 1:nrow(.))
 
   modelLinear <- dataToModel %>% lm(y ~ x, data = .)
 
   slope <- modelLinear %>% broom::tidy() %>% pull("estimate")
-  rs <- modelLinear %>%  broom::glance() %>% pull("r.squared")
+  rs <- modelLinear %>% broom::glance() %>% pull("r.squared")
 
   results <- data_frame(slopeLM = slope[2], interceptLM = slope[1], rsLM = rs)
 
@@ -78,30 +81,40 @@ slopeFinder <- function(emoDF){
   ## Thanks https://stats.stackexchange.com/questions/60994/fit-a-sinusoidal-term-to-data
   y <- dataToModel$y
   t <- dataToModel$x
-  res <- safely(nls)(y ~ A*sin(omega*t+phi)+C, data = data.frame(t,y),
-                     start = list(A = 1, omega = 1, phi = 1, C = 1))
-  res <- res$result
-  co <- coef(res)
 
-  fit <- function(x, a, b, c, d) {a*sin(b*x + c) + d}
+  # ## ############### I don't think I need the part below but get better fits when I keep it in???
+  # res <- safely(nls)(y ~ A * sin(omega * t + phi) + C, data = data.frame(t, y),
+  #   start = list(A = 1, omega = 1, phi = 1, C = 1))
+  # res <- res$result
+  # co <- coef(res)
+  # ## ^ #############
+
   ssp <- spectrum(y, plot = FALSE)
-  per <- 1/ssp$freq[ssp$spec == max(ssp$spec)]
-  reslm <- lm(y ~ sin(2*pi/per*t)+cos(2*pi/per*t))
+  per <- 1 / ssp$freq[ssp$spec == max(ssp$spec)]
+  reslm <- lm(y ~ sin(2 * pi / per * t) + cos(2 * pi / per * t))
   results$rsSIN <- broom::glance(reslm) %>% pull(r.squared)
+  # results <- bind_cols(results, co %>% data.frame %>% t %>% as.data.frame) ## This is the wrong stuff
+
   # rg <- diff(range(y)) # to plot
-  # plot(y~t,ylim=c(min(y)-0.1*rg,max(y)+0.1*rg))
-  # lines(fitted(reslm)~t,col=4,lty=2)
+  # plot(y~t,ylim=c(min(y)-0.1*rg,max(y)+0.1*rg)) ## Pretty suer this isn't required
 
+  SINreg <- broom::tidy(reslm) %>% select(estimate) %>% t %>% as.data.frame()
+  names(SINreg) <- broom::tidy(reslm) %>% pull(term)
 
+  results <- bind_cols(results, SINreg)
+  results$perSIN <- per
   ## getting class, first get max and then identify where it is
-  results <- results %>% mutate(mak=pmax(!!!rlang::syms(
-    grep(pattern = "rs", names(results), value = TRUE))))
+  results <- results %>% mutate(mak = pmax(!!!rlang::syms(
+    grep(pattern = "rs", names(results), value = TRUE)
+  )))
 
-  results <- results %>% mutate(category = case_when(
-    rsLM  == mak & slopeLM > 0 ~ '1.rags->riches',
-    rsLM  == mak & slopeLM < 0 ~ '2.riches->rags',
-    rsSIN == mak               ~ '3.boy meets girl'
-  )) %>% separate(category, c("catNum", "category"), sep = "\\.") %>%
+  results <- results %>%
+    mutate(category = case_when(
+      rsLM == mak & slopeLM > 0 ~ "1.rags->riches",
+      rsLM == mak & slopeLM < 0 ~ "2.riches->rags",
+      rsSIN == mak ~ "3.boy meets girl"
+    )) %>%
+    separate(category, c("catNum", "category"), sep = "\\.") %>%
     mutate(catNum = as.numeric(catNum))
 
   ## output
@@ -120,17 +133,17 @@ slopeFinder <- function(emoDF){
 #' @details DETAILS
 #' @examples
 #' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
+#' if (interactive()) {
+#'   # EXAMPLE1
+#' }
 #' }
 #' @rdname emoMultiPlotter
 #' @export
-emoMultiPlotter <- function(listOfEmos, titles = NULL, color = FALSE, showTrends = NULL){
+emoMultiPlotter <- function(listOfEmos, titles = NULL, color = FALSE, showTrends = NULL) {
   values <- listOfEmos %>% map("cumSentiment")
   if (color) colorpoints <- listOfEmos %>% map("color")
 
-  par(mfrow = c(4,ceiling(length(values)/4)), mar=c(2.1,2.1,2.1,2.1))
+  par(mfrow = c(4, ceiling(length(values) / 4)), mar = c(2.1, 2.1, 2.1, 2.1))
 
   for (i in seq_along(values)) {
     if (color) {
@@ -139,19 +152,30 @@ emoMultiPlotter <- function(listOfEmos, titles = NULL, color = FALSE, showTrends
       plot(values[[i]], type = "l")
     }
     ## Add titles or just numbers
-    if (is.null(titles)){
-      text(x=length(values[[i]])/2,y = max(values[[i]]),labels = i)
+    if (is.null(titles)) {
+      text(x = length(values[[i]]) / 2, y = max(values[[i]]), labels = i)
     } else {
-      text(x=length(values[[i]])/2,y = max(values[[i]]),labels = titles[i])
+      text(x = length(values[[i]]) / 2, y = max(values[[i]]), labels = titles[i])
     }
-    # browser()
+
     ## Add trends or not
     if (!is.null(showTrends)) {
-      if (showTrends$catNum[i] <= 2) abline(lsfit(x = seq_along(values[[i]]), y = values[[i]]),
-                                            col = "purple")
+      if (showTrends$catNum[i] <= 2) { ## LINEAR
+        abline(a = showTrends$interceptLM[i], b = showTrends$slopeLM[i],
+               col = "purple")
+      } else if (showTrends$catNum[i] == 3) { ## SIN
+        #lm(y ~ sin(2 * pi / per * t) + cos(2 * pi / per * t))
+        curve(showTrends$`sin(2 * pi/per * t)`[i]*sin(2 * pi / showTrends$perSIN[i]*x) +
+          showTrends$`cos(2 * pi/per * t)`[i]*cos(2*pi/showTrends$perSIN[i]*x), add = TRUE)
+
+        # lines(fitted(reslm)~t,col=4,lty=2) ## This wont work because I can't pass the reslm in a dataframe.
+
+        # curve(showTrends$A[i] * sin(showTrends$omega[i] * x + showTrends$phi[i]) + showTrends$C[i],
+        #       col = "purple", add = TRUE) ## This is the wrong stuff
+      }
     }
   }
-  par(mfrow = c(1,1))
+  par(mfrow = c(1, 1))
 }
 
 
@@ -164,22 +188,22 @@ emoMultiPlotter <- function(listOfEmos, titles = NULL, color = FALSE, showTrends
 #' @details DETAILS
 #' @examples
 #' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
+#' if (interactive()) {
+#'   # EXAMPLE1
+#' }
 #' }
 #' @rdname nrcMultiPlotter
 #' @export
-nrcMultiPlotter <- function(listOfEmos, titles = NULL){
-  values <- listOfEmos %>% map(select,contains("cum"))
+nrcMultiPlotter <- function(listOfEmos, titles = NULL) {
+  values <- listOfEmos %>% map(select, contains("cum"))
 
-  par(mfrow = c(4, ceiling(length(values)/4)), mar = c(2.1,2.1,2.1,2.1))
+  par(mfrow = c(4, ceiling(length(values) / 4)), mar = c(2.1, 2.1, 2.1, 2.1))
 
   for (i in seq_along(values)) {
-    plot(type = "l", values[[i]]$cumAnger, col = "red", lwd = 2, ylim = c(0,max(values[[i]])))
+    plot(type = "l", values[[i]]$cumAnger, col = "red", lwd = 2, ylim = c(0, max(values[[i]])))
     lines(type = "l", values[[i]]$cumAnticipation, lwd = 2, col = "grey")
     lines(type = "l", values[[i]]$cumDisgust, lwd = 2, col = "#cccc33")
-    lines(type = "l", values[[i]]$cumFear, lwd = 2, col = "yellow") #FFDB58
+    lines(type = "l", values[[i]]$cumFear, lwd = 2, col = "yellow") # FFDB58
     lines(type = "l", values[[i]]$cumJoy, lwd = 2, col = "purple")
     lines(type = "l", values[[i]]$cumSadness, lwd = 2, col = "black")
     lines(type = "l", values[[i]]$cumSurprise, lwd = 2, col = "orange")
@@ -187,14 +211,11 @@ nrcMultiPlotter <- function(listOfEmos, titles = NULL){
 
 
     ## Add titles or just numbers
-    if (is.null(titles)){
-      text(x=nrow(values[[i]])/2,y = max(values[[i]]),labels = i)
+    if (is.null(titles)) {
+      text(x = nrow(values[[i]]) / 2, y = max(values[[i]]), labels = i)
     } else {
-      text(x=nrow(values[[i]])/2,y = max(values[[i]]),labels = titles[i])
+      text(x = nrow(values[[i]]) / 2, y = max(values[[i]]), labels = titles[i])
     }
   }
-  par(mfrow = c(1,1))
+  par(mfrow = c(1, 1))
 }
-
-
-
--- otherStuff.R
+++ otherStuff.R
@@ -1,31 +1,28 @@
 
 library(tidyverse)
 
-# a <- file("C:/Users/Amit/Dropbox/Data/movie data/plots/plots")
-# aa <- file("C:/Users/Amit/Dropbox/Data/movie data/plots/titles")
-# b <- read_lines(a)
-# titles <- read_lines(aa) %>% head(27)
-# close(a)
-#
-# aa
-# bb <- head(b,1000)
-#
-# bb <- bb %>% paste(collapse = "") %>% str_split("<EOS>")
+a <- file("C:/Users/Amit/Dropbox/Data/movie data/plots/plots")
+aa <- file("C:/Users/Amit/Dropbox/Data/movie data/plots/titles")
+b <- read_lines(a)
+titles <- read_lines(aa) %>% head(27)
+close(a)
 
-dataset <- read_csv(file("http://bit.ly/2uhqjJE?.csv"))
-bb <- dataset$texts
-titles <- dataset %>% unite(name, FirstName, President) %>% pull
+aa
+bb <- head(b,1000)
+
+bb <- bb %>% paste(collapse = "") %>% str_split("<EOS>")
+bb <- bb[[1]]
+
+# dataset <- read_csv(file("http://bit.ly/2uhqjJE?.csv"))
+# bb <- dataset$texts
+# titles <- dataset %>% unite(name, FirstName, President) %>% pull
 
 ## Test
-bb[1] %>% emoDataframeMaker
-
-bb %>% head %>% map(emoDataframeMaker, addColor = TRUE)
-
 listOfEmos <- bb %>% map(emoDataframeMaker, addColor = TRUE, nrc = TRUE)
 listOfEmos
 
 ## Tests
-listOfEmos[[2]] %>% slopeFinder
+ttt <- listOfEmos[[1]] %>% slopeFinder
 slopes <- listOfEmos %>%  map_dfr(slopeFinder)
 slopes
 
